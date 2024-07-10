112330a7c50676e90373f63fb41c12448c6505b1
Sayan Maity <156517005+Sayancemk@users.noreply.github.com>
2024-07-10 05:32:47 +0000

    add all models

---
+++ Backend/models/order.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const orderSchema=new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:User,
+    },
+    book:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:Books,
+    },
+    status:{
+        type:String,
+        default:'Order Placed',
+        enum:['Ordered Placed','Out of Delivery','Delivered','Cancelled']
+    },
+},
+    {
+        timestamps:true
+    }
+
+)
+
+const Order=mongoose.model('order',orderSchema);
+
+export {
+    Order
+}
---
+++ Backend/models/user.model.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+    },
+    email:{
+        type:String,
+        requred:true,
+        unique:true,
+        trim:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    address:{
+        type:String,
+        required:true,
+    },
+    gender: {
+        type: String,
+        enum: ["F", "M", "O"]
+    },
+    avatar:{
+        type:String,
+        default:" https://avatar.iran.liara.run/public/boy?username=Ash",
+    },
+    role:{
+        type:String,
+        default:"user",
+        enum:["user","admin"]
+    },
+
+   
+    favourites:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Books'
+        },
+    ],
+    cart:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Books',
+        },
+],
+    orderHistory:[
+        {
+            type:mongoose.Schema.Types.ObjectId ,
+            ref:'Orders',
+        }
+],
+    
+ 
+},
+{timestamps:true}
+);
+const User=mongoose.model('user',userSchema);
+
+export{
+    User,
+}
