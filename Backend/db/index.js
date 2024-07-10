a51c897fb4e4a762e86acfde1537c3df8b848c6a
Sayan Maity <156517005+Sayancemk@users.noreply.github.com>
2024-07-10 05:30:32 +0000

    add all details

---
+++ Backend/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+import {DB_NAME,
+    PORT,
+    DB_URL} from '../constants.js';
+
+const connectDB= async()=>{
+
+        mongoose.connect(`${DB_URL}/${DB_NAME}`).then((connectionInstance)=>{
+                console.log(`MONGODB connected to ${DB_NAME} on port ${PORT}`)
+               
+        }).catch((error)=>{
+            console.log("MONGODB connection error",Error);
+            process.exit(1);
+        })
+}
+export default connectDB;
