79d348edd0c1fb99170f3c51f3bec5952bc04ae9
sayan maity <cse21109@cemk.ac.in>
2024-07-18 20:22:04 +0530

    chore: Update npm dependencies to latest versionss

--- Backend/controllers/bookController.js
+++ Backend/controllers/bookController.js
@@ -24,7 +24,7 @@ const createBooks=asyncHandler(async(req,resp)=>{
         author:req.body.author,
         price:req.body.price,
         desc:req.body.desc,
-        language:req.body.language34,
+        language:req.body.language,
     });
     const book=await newBook.save();
     if(!book){
